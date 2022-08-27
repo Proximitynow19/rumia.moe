cf1bf6bf0d468be4bc8db3101e4b452729b6c944
Jakob de Guzman <55227471+Proximitynow19@users.noreply.github.com>
2022-08-27 13:12:40 +1200

    very lazy landing page

---
+++ script.js
@@ -0,0 +1,29 @@
+const wallpapers = [
+  "250252.jpg",
+  "303025.jpg",
+  "303027.jpg",
+  "312921.jpg",
+  "715937.png",
+  "719612.png",
+  "754161.jpg",
+  "762597.jpg",
+  "763114.jpg",
+  "824372.png",
+  "847306.png",
+  "901837.png",
+  "914032.jpg",
+  "934750.png",
+  "937734.jpg",
+  "957023.png",
+  "1061397.jpg",
+  "1070378.png",
+  "1108003.png",
+  "1125361.png",
+  "1126546.png",
+  "1145963.jpg",
+  "1161086.jpg",
+];
+
+const index = Math.round(Math.random() * (wallpapers.length - 1));
+
+$(".hero").css("background-image", `url('wallpapers/${wallpapers[index]}')`);
